2aead1da91f0795e459d529bdccb1be1e92763cb
prog-yammer <367671@niuitmo.ru>
2025-04-14 04:58:35 +0300

    v1.1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    QTranslator translator;
+    if (translator.load(":/translations/ru.qm"))
+        app.installTranslator(&translator);
+
     MainWindow window;
     window.show();
     return app.exec();
---
+++ translations/ru.ts
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>GamePage</name>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="81"/>
+        <source>Draw! Nobody wins(</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="84"/>
+        <source>Opponent left! You win</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="87"/>
+        <source>Finish! %1 wins</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="91"/>
+        <source>Game ended</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LobbyPage</name>
+    <message>
+        <location filename="ui/game/lobby_page.cpp" line="47"/>
+        <source>This game is not available!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui/main_window.cpp" line="85"/>
+        <source>Connection lost!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QLabel</name>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="119"/>
+        <source>Your opponent: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="120"/>
+        <source>Waiting opponent...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="128"/>
+        <source>Your turn...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="129"/>
+        <source>Opponent&apos;s turn...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/lobby_page.cpp" line="20"/>
+        <source>Your nickname: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/lobby_page.cpp" line="23"/>
+        <source>Create game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/lobby_page.cpp" line="27"/>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="ui/game/lobby_page.cpp" line="46"/>
+        <location filename="ui/main_window.cpp" line="84"/>
+        <source>Notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QPushButton</name>
+    <message>
+        <location filename="ui/auth/login_page.cpp" line="18"/>
+        <source>Enter your nickname</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Auth</source>
+        <translation type="unfinished">Войти</translation>
+    </message>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="13"/>
+        <source>Game id: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/game/game_page.cpp" line="17"/>
+        <source>Leave game</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
